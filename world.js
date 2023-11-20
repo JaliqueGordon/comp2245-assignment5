c98e18afd818e6130d3fcb6fdd74e62209986a19
Jalique Gordon <jaliquegordon@gmail.com>
2023-11-19 21:28:17 -0400

    Completed Exercise 5

--- world.js
+++ world.js
@@ -1,27 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
     var lookupButton = document.getElementById("lookup");
+    var lookupCitiesButton = document.getElementById("lookupCities");
+
+    lookupButton.addEventListener("click", function() {
+        fetchData("countries");
+
+    });
+
+    lookupCitiesButton.addEventListener("click", function() {
+        fetchData("cities");
+
+    });
+
+
+    function fetchData(lookupType) {
+        var countryInput = document.getElementById("country");
+        var country = countryInput.value;
+
+        var url = "world.php?country=" + country + "&lookup=" + lookupType;
+
+        fetch (url)
+              .then(response => response.text())
+              .then(data =>  {
+                  document.getElementById("result").innerHTML = data;
+
+              })
+              .catch = (error => {
+                console.error("Error fetching data:", error);
+
+              });
+        
+    }
+
+
+});
+
+
+
 
     
 
 
  
-
-    lookupButton.addEventListener("click", function() {
-        var countryInput = document.getElementById("country");
-        var country = countryInput.value
-
-
-        fetch("world.php?country=" + country)
-             .then(response => response.text())
-             .then(data => {
-                document.getElementById("result").innerHTML = data;
-
-             })
-
-             .catch(error => {
-                console.error("Error fetching data:", error);
-
-             });
-  });
-
-    });
--- world.php
+++ world.php
@@ -7,9 +7,28 @@ $dbname = 'world';
 
 
 $country = isset($_GET['country']) ? $_GET['country'] : '';
+$lookupType = isset($_GET['lookup']) ? $_GET['lookup'] : '';
+
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+
+if ($lookupType === 'cities') {
+  $stmt = $conn->prepare("
+  SELECT cities.name AS cityName, cities.district, cities.population 
+  FROM cities
+  JOIN countries ON cities.country_code = countries.code
+  WHERE countries.name LIKE :country
+  ");
+
+} else {
+  $stmt = $conn->prepare("
+  SELECT name, continent, independence_year, head_of_state
+  FROM countries
+  WHERE name LIKE :country
+  ");
+}
+
+
 $stmt->bindParam(':country', $country, PDO::PARAM_STR);
 $stmt->execute();
 
@@ -17,14 +36,30 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 echo "<table border='1'>";
-echo "<tr><th>Country Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
-foreach ($results as $row) {
+if ($lookupType == 'cities') {
+  echo "<tr><th>Country Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
+  foreach ($results as $row) {
     echo "<tr>";
-    echo "<td>{$row['name']}</td>";
-    echo "<td>{$row['continent']}</td>";
-    echo "<td>{$row['independence_year']}</td>";
-    echo "<td>{$row['head_of_state']}</td>";
+    echo "<td>{$row['cityName']}</td>";
+    echo "<td>{$row['district']}</td>";
+    echo "<td>{$row['population']}</td>";
     echo "</tr>";
+
+}
+
+} else {
+  echo "<tr><th>Country Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
+  foreach ($results as $row) {
+      echo "<tr>";
+      echo "<td>{$row['name']}</td>";
+      echo "<td>{$row['continent']}</td>";
+      echo "<td>{$row['independence_year']}</td>";
+      echo "<td>{$row['head_of_state']}</td>";
+      echo "</tr>";
+  }
 }
 echo "</table>";
 ?>
+
+
+
